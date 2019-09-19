49258d69de3cc99b8d9cf05bda00cb8c17b9ccd3
Guilherme Santos Rodrigues <guilherme.sogliato@gmail.com>
2019-09-19 09:30:19 -0300

    Adding mail process to backround with queues

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -9,7 +9,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -138,18 +139,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      // text: 'Você tem um serviço cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
